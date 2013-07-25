eb75e0c1a9762977f4f46f0faabd8b5897665b4a
Fabio Utzig <fabio@utzig.net>
2013-07-25 20:51:28 -0300

    Add pal_default_config for freedom board

--- boards/FREESCALE_FREEDOM_KL25Z/board.c
+++ boards/FREESCALE_FREEDOM_KL25Z/board.c
@@ -25,18 +25,88 @@
  */
 const PALConfig pal_default_config =
 {
-#if 0
-  {VAL_GPIOA_MODER, VAL_GPIOA_OTYPER, VAL_GPIOA_OSPEEDR, VAL_GPIOA_PUPDR,
-   VAL_GPIOA_ODR,   VAL_GPIOA_AFRL,   VAL_GPIOA_AFRH},
-  {VAL_GPIOB_MODER, VAL_GPIOB_OTYPER, VAL_GPIOB_OSPEEDR, VAL_GPIOB_PUPDR,
-   VAL_GPIOB_ODR,   VAL_GPIOB_AFRL,   VAL_GPIOB_AFRH},
-  {VAL_GPIOC_MODER, VAL_GPIOC_OTYPER, VAL_GPIOC_OSPEEDR, VAL_GPIOC_PUPDR,
-   VAL_GPIOC_ODR,   VAL_GPIOC_AFRL,   VAL_GPIOC_AFRH},
-  {VAL_GPIOD_MODER, VAL_GPIOD_OTYPER, VAL_GPIOD_OSPEEDR, VAL_GPIOD_PUPDR,
-   VAL_GPIOD_ODR,   VAL_GPIOD_AFRL,   VAL_GPIOD_AFRH},
-  {VAL_GPIOF_MODER, VAL_GPIOF_OTYPER, VAL_GPIOF_OSPEEDR, VAL_GPIOF_PUPDR,
-   VAL_GPIOF_ODR,   VAL_GPIOF_AFRL,   VAL_GPIOF_AFRH}
-#endif
+  .ports = {
+    {
+      .port = IOPORT1,
+      .pads = {
+        PAL_MODE_RESET,           PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,           PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,           PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,           PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,           PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,           PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_UNCONNECTED,     PAL_MODE_UNCONNECTED,       PAL_MODE_RESET,
+        PAL_MODE_RESET,           PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,           PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,           PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,           PAL_MODE_RESET,
+      },
+    },
+    {
+      .port = IOPORT2,
+      .pads = {
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_OUTPUT_PUSHPULL,   PAL_MODE_OUTPUT_PUSHPULL,   PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,
+      },
+    },
+    {
+      .port = IOPORT3,
+      .pads = {
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,
+      },
+    },
+    {
+      .port = IOPORT4,
+      .pads = {
+        PAL_MODE_RESET,             PAL_MODE_OUTPUT_PUSHPULL,   PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,
+      },
+    },
+    {
+      .port = IOPORT5,
+      .pads = {
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,             PAL_MODE_RESET,
+        PAL_MODE_RESET,             PAL_MODE_RESET,
+      },
+    },
+  },
 };
 #endif
 
