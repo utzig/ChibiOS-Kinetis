4f9d1878b2cd2f376f8e31e40e4ea6f873dcca07
Fabio Utzig <fabio@utzig.net>
2013-07-24 23:22:30 -0300

    Add CMSIS configs and more subsystems

--- os/hal/platforms/KL2x/kl25z.h
+++ os/hal/platforms/KL2x/kl25z.h
@@ -1,8 +1,74 @@
 #ifndef _KL25Z_H_
 #define _KL25Z_H_
 
-typedef struct {
+/*
+ * ==============================================================
+ * ---------- Interrupt Number Definition -----------------------
+ * ==============================================================
+ */
+typedef enum IRQn
+{
+/******  Cortex-M0 Processor Exceptions Numbers ****************/
+  Reset_IRQn                    = -15,
+  NonMaskableInt_IRQn           = -14,
+  HardFault_IRQn                = -13,
+  SVCall_IRQn                   = -5,
+  PendSV_IRQn                   = -2,
+  SysTick_IRQn                  = -1,
 
+/******  KL2x Specific Interrupt Numbers ***********************/
+  DMA0_IRQn                     = 0,
+  DMA1_IRQn                     = 1,
+  DMA2_IRQn                     = 2,
+  DMA3_IRQn                     = 3,
+  Reserved0_IRQn                = 4,
+  FTFA_IRQn                     = 5,
+  PMC_IRQn                      = 6,
+  LLWU_IRQn                     = 7,
+  I2C0_IRQn                     = 8,
+  I2C1_IRQn                     = 9,
+  SPI0_IRQn                     = 10,
+  SPI1_IRQn                     = 11,
+  UART0_IRQn                    = 12,
+  UART1_IRQn                    = 13,
+  UART2_IRQn                    = 14,
+  ADC0_IRQn                     = 15,
+  CMP0_IRQn                     = 16,
+  TMP0_IRQn                     = 17,
+  TMP1_IRQn                     = 18,
+  TMP2_IRQn                     = 19,
+  RTC0_IRQn                     = 20,
+  RTC1_IRQn                     = 21,
+  PIT_IRQn                      = 22,
+  Reserved1_IRQn                = 23,
+  USB_OTG_IRQn                  = 24,
+  DAC0_IRQn                     = 25,
+  TSI0_IRQn                     = 26,
+  MCG_IRQn                      = 27,
+  LPTMR0_IRQn                   = 28,
+  Reserved2_IRQn                = 29,
+  PINA_IRQn                     = 30,
+  PIND_IRQn                     = 31,
+} IRQn_Type;
+
+/*
+ * ==========================================================================
+ * ----------- Processor and Core Peripheral Section ------------------------
+ * ==========================================================================
+ */
+
+/**
+ * @brief STM32F0xx Interrupt Number Definition, according to the selected device
+ *        in @ref Library_configuration_section
+ */
+#define __MPU_PRESENT             0 /*!< STM32F0xx do not provide MPU                  */
+#define __NVIC_PRIO_BITS          2 /*!< STM32F0xx uses 2 Bits for the Priority Levels */
+#define __Vendor_SysTickConfig    0 /*!< Set to 1 if different SysTick Config is used  */
+
+#include "core_cm0plus.h"           /* Cortex-M0+ processor and core peripherals       */
+
+typedef struct
+{
   __IO uint32_t SOPT1;
   __IO uint32_t SOPT1CFG;
        uint32_t RESERVED0[1023];
@@ -30,52 +96,19 @@ typedef struct {
        uint32_t RESERVED7[39];
   __IO uint32_t COPC;
   __IO uint32_t SRVCOP;
-
 } SIM_TypeDef;
 
-typedef struct {
-
-  __IO uint32_t PCR0;
-  __IO uint32_t PCR1;
-  __IO uint32_t PCR2;
-  __IO uint32_t PCR3;
-  __IO uint32_t PCR4;
-  __IO uint32_t PCR5;
-  __IO uint32_t PCR6;
-  __IO uint32_t PCR7;
-  __IO uint32_t PCR8;
-  __IO uint32_t PCR9;
-  __IO uint32_t PCR10;
-  __IO uint32_t PCR11;
-  __IO uint32_t PCR12;
-  __IO uint32_t PCR13;
-  __IO uint32_t PCR14;
-  __IO uint32_t PCR15;
-  __IO uint32_t PCR16;
-  __IO uint32_t PCR17;
-  __IO uint32_t PCR18;
-  __IO uint32_t PCR19;
-  __IO uint32_t PCR20;
-  __IO uint32_t PCR21;
-  __IO uint32_t PCR22;
-  __IO uint32_t PCR23;
-  __IO uint32_t PCR24;
-  __IO uint32_t PCR25;
-  __IO uint32_t PCR26;
-  __IO uint32_t PCR27;
-  __IO uint32_t PCR28;
-  __IO uint32_t PCR29;
-  __IO uint32_t PCR30;
-  __IO uint32_t PCR31;
+typedef struct
+{
+  __IO uint32_t PCR[32];
   __IO uint32_t GPCLR;
   __IO uint32_t GPCHR;
        uint32_t RESERVED0[6];
   __IO uint32_t ISFR;
-
 } PORT_TypeDef;
 
-typedef struct {
-
+typedef struct
+{
   __IO uint8_t  C1;
   __IO uint8_t  C2;
   __IO uint8_t  C3;
@@ -92,37 +125,117 @@ typedef struct {
   __IO uint8_t  C8;
   __IO uint8_t  C9;
   __IO uint8_t  C10;
-
 } MCG_TypeDef;
 
-typedef struct {
-
-  __IO uint32_t CR;
-
+typedef struct
+{
+  __IO uint8_t  CR;
 } OSC_TypeDef;
+
+typedef struct
+{
+  __IO uint32_t SC;
+  __IO uint32_t CNT;
+  __IO uint32_t MOD;
+  __IO uint32_t C0SC;
+  __IO uint32_t C0V;
+  __IO uint32_t C1SC;
+  __IO uint32_t C1V;
+  __IO uint32_t C2SC;
+  __IO uint32_t C2V;
+  __IO uint32_t C3SC;
+  __IO uint32_t C3V;
+  __IO uint32_t C4SC;
+  __IO uint32_t C4V;
+  __IO uint32_t C5SC;
+  __IO uint32_t C5V;
+       uint32_t RESERVED0[5];
+  __IO uint32_t STATUS;
+       uint32_t RESERVED1[12];
+  __IO uint32_t CONF;
+} TPM_TypeDef;
+
+typedef struct
+{
+  __IO uint32_t PDOR;
+  __IO uint32_t PSOR;
+  __IO uint32_t PCOR;
+  __IO uint32_t PTOR;
+  __IO uint32_t PDIR;
+  __IO uint32_t PDDR;
+} GPIO_TypeDef;
 
 /****************************************************************/
 /*                  Peripheral memory map                       */
 /****************************************************************/
-#define SIM_BASE                0x40047000
-#define PORTA_BASE              0x40049000
-#define PORTB_BASE              0x4004A000
-#define PORTC_BASE              0x4004B000
-#define PORTD_BASE              0x4004C000
-#define PORTE_BASE              0x4004D000
-#define MCG_BASE                0x40064000
-#define OSC_BASE                0x40065000
+#define TPM0_BASE               ((uint32_t)0x40038000)
+#define TPM1_BASE               ((uint32_t)0x40039000)
+#define TPM2_BASE               ((uint32_t)0x4003A000)
+#define SIM_BASE                ((uint32_t)0x40047000)
+#define PORTA_BASE              ((uint32_t)0x40049000)
+#define PORTB_BASE              ((uint32_t)0x4004A000)
+#define PORTC_BASE              ((uint32_t)0x4004B000)
+#define PORTD_BASE              ((uint32_t)0x4004C000)
+#define PORTE_BASE              ((uint32_t)0x4004D000)
+#define MCG_BASE                ((uint32_t)0x40064000)
+#define OSC0_BASE               ((uint32_t)0x40065000)
+#define GPIOA_BASE              ((uint32_t)0x400FF000)
+#define GPIOB_BASE              ((uint32_t)0x400FF040)
+#define GPIOC_BASE              ((uint32_t)0x400FF080)
+#define GPIOD_BASE              ((uint32_t)0x400FF0C0)
+#define GPIOE_BASE              ((uint32_t)0x400FF100)
 
 /****************************************************************/
 /*                 Peripheral declaration                       */
 /****************************************************************/
-#define SIM                     ((SIM_TypeDef  *)  SIM_BASE)
-#define PORTA                   ((PORT_TypeDef  *) PORTA_BASE)
-#define PORTB                   ((PORT_TypeDef  *) PORTB_BASE)
-#define PORTC                   ((PORT_TypeDef  *) PORTC_BASE)
-#define PORTD                   ((PORT_TypeDef  *) PORTD_BASE)
-#define PORTE                   ((PORT_TypeDef  *) PORTE_BASE)
-#define MCG                     ((MCG_TypeDef  *)  MCG_BASE)
-#define OSC                     ((OSC_TypeDef  *)  OSC_BASE)
+#define TPM0                    ((volatile TPM_TypeDef *)   TPM0_BASE)
+#define TPM1                    ((volatile TPM_TypeDef *)   TPM1_BASE)
+#define TPM2                    ((volatile TPM_TypeDef *)   TPM2_BASE)
+#define SIM                     ((volatile SIM_TypeDef  *)  SIM_BASE)
+#define PORTA                   ((volatile PORT_TypeDef  *) PORTA_BASE)
+#define PORTB                   ((volatile PORT_TypeDef  *) PORTB_BASE)
+#define PORTC                   ((volatile PORT_TypeDef  *) PORTC_BASE)
+#define PORTD                   ((volatile PORT_TypeDef  *) PORTD_BASE)
+#define PORTE                   ((volatile PORT_TypeDef  *) PORTE_BASE)
+#define MCG                     ((volatile MCG_TypeDef  *)  MCG_BASE)
+#define OSC0                    ((volatile OSC_TypeDef  *)  OSC0_BASE)
+#define GPIOA                   ((volatile GPIO_TypeDef  *) GPIOA_BASE)
+#define GPIOB                   ((volatile GPIO_TypeDef  *) GPIOB_BASE)
+#define GPIOC                   ((volatile GPIO_TypeDef  *) GPIOC_BASE)
+#define GPIOD                   ((volatile GPIO_TypeDef  *) GPIOD_BASE)
+#define GPIOE                   ((volatile GPIO_TypeDef  *) GPIOE_BASE)
+
+/****************************************************************/
+/*           Peripheral Registers Bits Definition               */
+/****************************************************************/
+
+/****************************************************************/
+/*                                                              */
+/*             System Integration Module (SIM)                  */
+/*                                                              */
+/****************************************************************/
+/*********  Bits definition for SIM_SOPT1 register  *************/
+#define SIM_SOPT1_USBREGEN           ((uint32_t)0x80000000)    /*!< USB voltage regulator enable */
+#define SIM_SOPT1_USBSSTBY           ((uint32_t)0x40000000)    /*!< USB voltage regulator in standby mode during Stop, VLPS, LLS and VLLS modes */
+#define SIM_SOPT1_USBVSTBY           ((uint32_t)0x20000000)    /*!< USB voltage regulator in standby mode during VLPR and VLPW modes */
+#define SIM_SOPT1_OSC32KSEL          ((uint32_t)0x000C0000)    /*!< 32K oscillator clock select */
+
+/*******  Bits definition for SIM_SOPT1CFG register  ************/
+#define SIM_SOPT1CFG_USSWE           ((uint32_t)0x04000000)    /*!< USB voltage regulator stop standby write enable */
+#define SIM_SOPT1CFG_UVSWE           ((uint32_t)0x02000000)    /*!< USB voltage regulator VLP standby write enable */
+#define SIM_SOPT1CFG_URWE            ((uint32_t)0x01000000)    /*!< USB voltage regulator voltage regulator write enable */
+
+/****************************************************************/
+/*                                                              */
+/*                   Oscillator (OSC)                           */
+/*                                                              */
+/****************************************************************/
+/***********  Bits definition for OSC_CR register  **************/
+#define OSC_CR_ERCLKEN               ((uint8_t)0x80)    /*!< External Reference Enable */
+#define OSC_CR_EREFSTEN              ((uint8_t)0x20)    /*!< External Reference Stop Enable */
+#define OSC_CR_SC2P                  ((uint8_t)0x08)    /*!< Oscillator 2pF Capacitor Load Configure */
+#define OSC_CR_SC4P                  ((uint8_t)0x04)    /*!< Oscillator 4pF Capacitor Load Configure */
+#define OSC_CR_SC8P                  ((uint8_t)0x02)    /*!< Oscillator 8pF Capacitor Load Configure */
+#define OSC_CR_SC16P                 ((uint8_t)0x01)    /*!< Oscillator 16pF Capacitor Load Configure */
 
 #endif
