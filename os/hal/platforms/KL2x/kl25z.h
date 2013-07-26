64e188689cd16fefaaf8da8fe7bfb14388bb9bdb
Fabio Utzig <fabio@utzig.net>
2013-07-25 22:08:09 -0300

    Add UART bit definitions

--- os/hal/platforms/KL2x/kl25z.h
+++ os/hal/platforms/KL2x/kl25z.h
@@ -299,4 +299,101 @@ typedef struct
 #define OSC_CR_SC8P                  ((uint8_t)0x02)    /*!< Oscillator 8pF Capacitor Load Configure */
 #define OSC_CR_SC16P                 ((uint8_t)0x01)    /*!< Oscillator 16pF Capacitor Load Configure */
 
+/****************************************************************/
+/*                                                              */
+/*     Universal Asynchronous Receiver/Transmitter (UART)       */
+/*                                                              */
+/****************************************************************/
+/*********  Bits definition for UARTx_BDH register  *************/
+#define UARTx_BDH_LBKDIE             ((uint8_t)0x80)    /*!< LIN Break Detect Interrupt Enable */
+#define UARTx_BDH_RXEDGIE            ((uint8_t)0x40)    /*!< RX Input Active Edge Interrupt Enable */
+#define UARTx_BDH_SBNS               ((uint8_t)0x20)    /*!< Stop Bit Number Select */
+#define UARTx_BDH_SBR                ((uint8_t)0x1F)    /*!< Baud Rate Modulo Divisor */
+
+/*********  Bits definition for UARTx_BDL register  *************/
+#define UARTx_BDL_SBR                ((uint8_t)0xFF)    /*!< Baud Rate Modulo Divisor */
+
+/*********  Bits definition for UARTx_C1 register  **************/
+#define UARTx_C1_LOOPS               ((uint8_t)0x80)    /*!< Loop Mode Select */
+#define UARTx_C1_DOZEEN              ((uint8_t)0x40)    /*!< Doze Enable */
+#define UARTx_C1_UARTSWAI            ((uint8_t)0x40)    /*!< UART Stops in Wait Mode */
+#define UARTx_C1_RSRC                ((uint8_t)0x20)    /*!< Receiver Source Select */
+#define UARTx_C1_M                   ((uint8_t)0x10)    /*!< 9-Bit or 8-Bit Mode Select */
+#define UARTx_C1_WAKE                ((uint8_t)0x08)    /*!< Receiver Wakeup Method Select */
+#define UARTx_C1_ILT                 ((uint8_t)0x04)    /*!< Idle Line Type Select */
+#define UARTx_C1_PE                  ((uint8_t)0x02)    /*!< Parity Enable */
+#define UARTx_C1_PT                  ((uint8_t)0x01)    /*!< Parity Type */
+
+/*********  Bits definition for UARTx_C2 register  **************/
+#define UARTx_C2_TIE                 ((uint8_t)0x80)    /*!< Transmit Interrupt Enable for TDRE */
+#define UARTx_C2_TCIE                ((uint8_t)0x40)    /*!< Transmission Complete Interrupt Enable for TC */
+#define UARTx_C2_RIE                 ((uint8_t)0x20)    /*!< Receiver Interrupt Enable for RDRF */
+#define UARTx_C2_ILIE                ((uint8_t)0x10)    /*!< Idle Line Interrupt Enable for IDLE */
+#define UARTx_C2_TE                  ((uint8_t)0x08)    /*!< Transmitter Enable */
+#define UARTx_C2_RE                  ((uint8_t)0x04)    /*!< Receiver Enable */
+#define UARTx_C2_RWU                 ((uint8_t)0x02)    /*!< Receiver Wakeup Control */
+#define UARTx_C2_SBK                 ((uint8_t)0x01)    /*!< Send Break */
+
+/*********  Bits definition for UARTx_S1 register  **************/
+#define UARTx_S1_TDRE                ((uint8_t)0x80)    /*!< Transmit Data Register Empty Flag */
+#define UARTx_S1_TC                  ((uint8_t)0x40)    /*!< Transmission Complete Flag */
+#define UARTx_S1_RDRF                ((uint8_t)0x20)    /*!< Receiver Data Register Full Flag */
+#define UARTx_S1_IDLE                ((uint8_t)0x10)    /*!< Idle Line Flag */
+#define UARTx_S1_OR                  ((uint8_t)0x08)    /*!< Receiver Overrun Flag */
+#define UARTx_S1_NF                  ((uint8_t)0x04)    /*!< Noise Flag */
+#define UARTx_S1_FE                  ((uint8_t)0x02)    /*!< Framing Error Flag */
+#define UARTx_S1_PF                  ((uint8_t)0x01)    /*!< Parity Error Flag */
+
+/*********  Bits definition for UARTx_S2 register  **************/
+#define UARTx_S2_LBKDIF              ((uint8_t)0x80)    /*!< LIN Break Detect Interrupt Flag */
+#define UARTx_S2_RXEDGIF             ((uint8_t)0x40)    /*!< UART_RX Pin Active Edge Interrupt Flag */
+#define UARTx_S2_MSBF                ((uint8_t)0x20)    /*!< MSB First */
+#define UARTx_S2_RXINV               ((uint8_t)0x10)    /*!< Receive Data Inversion */
+#define UARTx_S2_RWUID               ((uint8_t)0x08)    /*!< Receive Wake Up Idle Detect */
+#define UARTx_S2_BRK13               ((uint8_t)0x04)    /*!< Break Character Generation Length */
+#define UARTx_S2_LBKDE               ((uint8_t)0x02)    /*!< LIN Break Detect Enable */
+#define UARTx_S2_RAF                 ((uint8_t)0x01)    /*!< Receiver Active Flag */
+
+/*********  Bits definition for UARTx_C3 register  **************/
+#define UARTx_C3_R8T9                ((uint8_t)0x80)    /*!< Receive Bit 8 / Transmit Bit 9 */
+#define UARTx_C3_R8                  ((uint8_t)0x80)    /*!< Ninth Data Bit for Receiver */
+#define UARTx_C3_R9T8                ((uint8_t)0x40)    /*!< Receive Bit 9 / Transmit Bit 8 */
+#define UARTx_C3_T8                  ((uint8_t)0x40)    /*!< Ninth Data Bit for Transmitter */
+#define UARTx_C3_TXDIR               ((uint8_t)0x20)    /*!< UART_TX Pin Direction in Single-Wire Mode */
+#define UARTx_C3_TXINV               ((uint8_t)0x10)    /*!< Transmit Data Inversion */
+#define UARTx_C3_ORIE                ((uint8_t)0x08)    /*!< Overrun Interrupt Enable */
+#define UARTx_C3_NEIE                ((uint8_t)0x04)    /*!< Noise Error Interrupt Enable */
+#define UARTx_C3_FEIE                ((uint8_t)0x02)    /*!< Framing Error Interrupt Enable */
+#define UARTx_C3_PEIE                ((uint8_t)0x01)    /*!< Parity Error Interrupt Enable */
+
+/*********  Bits definition for UARTx_D register  ***************/
+#define UARTx_D_R7T7                 ((uint8_t)0x80)    /*!< Read receive data buffer 7 or write transmit data buffer 7 */
+#define UARTx_D_R6T6                 ((uint8_t)0x40)    /*!< Read receive data buffer 6 or write transmit data buffer 6 */
+#define UARTx_D_R5T5                 ((uint8_t)0x20)    /*!< Read receive data buffer 5 or write transmit data buffer 5 */
+#define UARTx_D_R4T4                 ((uint8_t)0x10)    /*!< Read receive data buffer 4 or write transmit data buffer 4 */
+#define UARTx_D_R3T3                 ((uint8_t)0x08)    /*!< Read receive data buffer 3 or write transmit data buffer 3 */
+#define UARTx_D_R2T2                 ((uint8_t)0x04)    /*!< Read receive data buffer 2 or write transmit data buffer 2 */
+#define UARTx_D_R1T1                 ((uint8_t)0x02)    /*!< Read receive data buffer 1 or write transmit data buffer 1 */
+#define UARTx_D_R0T0                 ((uint8_t)0x01)    /*!< Read receive data buffer 0 or write transmit data buffer 0 */
+
+/*********  Bits definition for UARTx_MA1 register  *************/
+#define UARTx_MA1_MA                 ((uint8_t)0xFF)    /*!< Match Address */
+
+/*********  Bits definition for UARTx_MA2 register  *************/
+#define UARTx_MA2_MA                 ((uint8_t)0xFF)    /*!< Match Address */
+
+/*********  Bits definition for UARTx_C4 register  **************/
+#define UARTx_C4_MAEN1               ((uint8_t)0x80)    /*!< Match Address Mode Enable 1 */
+#define UARTx_C4_TDMAS               ((uint8_t)0x80)    /*!< Transmitter DMA Select */
+#define UARTx_C4_MAEN2               ((uint8_t)0x40)    /*!< Match Address Mode Enable 2 */
+#define UARTx_C4_M10                 ((uint8_t)0x20)    /*!< 10-bit Mode Select */
+#define UARTx_C4_RDMAS               ((uint8_t)0x80)    /*!< Receiver Full DMA Select */
+#define UARTx_C4_OSR                 ((uint8_t)0x1F)    /*!< Over Sampling Ratio */
+
+/*********  Bits definition for UARTx_C5 register  **************/
+#define UARTx_C5_TDMAE               ((uint8_t)0x80)    /*!< Transmitter DMA Enable */
+#define UARTx_C5_RDMAE               ((uint8_t)0x20)    /*!< Receiver Full DMA Enable */
+#define UARTx_C5_BOTHEDGE            ((uint8_t)0x02)    /*!< Both Edge Sampling */
+#define UARTx_C5_RESYNCDIS           ((uint8_t)0x01)    /*!< Resynchronization Disable */
+
 #endif
