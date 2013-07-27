24de76717e9bce03f361b4343c8a03424d705a15
Fabio Utzig <fabio@utzig.net>
2013-07-27 11:55:16 -0300

    Update main demo test

--- demos/ARMCM0P-FREEDOM-KL25Z/main.c
+++ demos/ARMCM0P-FREEDOM-KL25Z/main.c
@@ -16,52 +16,41 @@
 
 #include "ch.h"
 #include "hal.h"
-//#include "test.h"
+#include "test.h"
 
-#if 0
-/*
- * Blue LED blinker thread, times are in milliseconds.
- */
-static WORKING_AREA(waThread1, 128);
-static msg_t Thread1(void *arg) {
-
+static WORKING_AREA(waThread1, 64);
+static msg_t Thread1(void *arg)
+{
   (void)arg;
-  chRegSetThreadName("blinker1");
+  chRegSetThreadName("RedBlinker");
   while (TRUE) {
-    //palClearPad(GPIOC, GPIOC_LED4);
-    chThdSleepMilliseconds(500);
-    //palSetPad(GPIOC, GPIOC_LED4);
-    chThdSleepMilliseconds(500);
+    palTogglePad(IOPORT2, 18);
+    chThdSleepMilliseconds(300);
   }
 }
 
-/*
- * Green LED blinker thread, times are in milliseconds.
- */
-static WORKING_AREA(waThread2, 128);
-static msg_t Thread2(void *arg) {
-
+static WORKING_AREA(waThread2, 64);
+static msg_t Thread2(void *arg)
+{
   (void)arg;
-  chRegSetThreadName("blinker2");
+  chRegSetThreadName("GreenBlinker");
   while (TRUE) {
-    //palClearPad(GPIOC, GPIOC_LED3);
-    chThdSleepMilliseconds(250);
-    //palSetPad(GPIOC, GPIOC_LED3);
-    chThdSleepMilliseconds(250);
+    palTogglePad(IOPORT2, 19);
+    chThdSleepMilliseconds(600);
   }
 }
-#endif
 
-static void RGB_LED(uint8_t r, uint8_t g, uint8_t b) {
-  if (r) GPIOB->PCOR |= (1 << 18);
-  else GPIOB->PSOR |= (1 << 18);
-
-  if (g) GPIOB->PCOR |= (1 << 19);
-  else GPIOB->PSOR |= (1 << 19);
-
-  if (b) GPIOD->PCOR |= (1 << 1);
-  else GPIOD->PSOR |= (1 << 1);
+static WORKING_AREA(waThread3, 64);
+static msg_t Thread3(void *arg)
+{
+  (void)arg;
+  chRegSetThreadName("BlueBlinker");
+  while (TRUE) {
+    palTogglePad(IOPORT4, 1);
+    chThdSleepMilliseconds(900);
+  }
 }
+
 /*
  * Application entry point.
  */
@@ -78,33 +67,19 @@ int main(void) {
   chSysInit();
 
   /*
-   * Activates the serial driver 1 using the driver default configuration.
-   * PA9 and PA10 are routed to USART1.
+   * Activates serial 1 (UART0) using the driver default configuration.
    */
-  //sdStart(&SD1, NULL);
-  //palSetPadMode(GPIOA, 9, PAL_MODE_ALTERNATE(1));       /* USART1 TX.       */
-  //palSetPadMode(GPIOA, 10, PAL_MODE_ALTERNATE(1));      /* USART1 RX.       */
+  sdStart(&SD1, NULL);
 
   /*
    * Creates the blinker threads.
    */
-  //chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
-  //chThdCreateStatic(waThread2, sizeof(waThread2), NORMALPRIO, Thread2, NULL);
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+  chThdCreateStatic(waThread2, sizeof(waThread2), NORMALPRIO, Thread2, NULL);
+  chThdCreateStatic(waThread3, sizeof(waThread3), NORMALPRIO, Thread3, NULL);
 
-  /*
-   * Normal main() thread activity, in this demo it does nothing except
-   * sleeping in a loop and check the button state, when the button is
-   * pressed the test procedure is launched with output on the serial
-   * driver 1.
-   */
+  TestThread(&SD1);
   while (TRUE) {
-    //if (palReadPad(GPIOA, GPIOA_BUTTON))
-      //TestThread(&SD1);
-    RGB_LED(1, 0, 0);
-    chThdSleepMilliseconds(500);
-    RGB_LED(0, 1, 0);
-    chThdSleepMilliseconds(500);
-    RGB_LED(0, 0, 1);
     chThdSleepMilliseconds(500);
   }
 }
